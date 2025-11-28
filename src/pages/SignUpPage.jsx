4112338f297cdf1715ac3d04a9680d2ac095bb81
Oleksandr Bakai <saniabakay@gmail.com>
2025-11-28 21:14:50 +0100

    28.11.25

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,39 +1,18 @@
-import { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { postData } from "../functions/api.functions";
+import {useState, useContext} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {postData} from '../functions/api.functions';
 const VITE_API_URL = import.meta.env.VITE_API_URL;
-import { LanguageContext } from "../context.languages/LanguageContext";
+import {LanguageContext} from '../context.languages/LanguageContext';
 
 export default function SignupPage() {
-  const { t } = useContext(LanguageContext);
+  const {t} = useContext(LanguageContext);
 
-  const [email, setEmail] = useState("");
-  const [code, setCode] = useState("");
-  const [isCodeSent, setIsCodeSent] = useState(false);
-  const [isVerified, setIsVerified] = useState(false);
+  const [email, setEmail] = useState('');
 
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
   const navigate = useNavigate();
-
-  // Send code to email
-  async function sendCode() {
-    const res = await postData(`${VITE_API_URL}/emailauth/send-email-code`, { email });
-    if (res.success) {
-      alert(`${t.code_sent}`);
-      setIsCodeSent(true);
-    }
-  }
-
-  // Verify code from email
-  async function verifyCode() {
-    const res = await postData(`${VITE_API_URL}/emailauth/verify-email-code`, { email, code });
-    if (res.success) {
-      alert(`${t.email_confirmed}`);
-      setIsVerified(true);
-    }
-  }
 
   // Final registration
   async function handleSignupSubmit(e) {
@@ -43,11 +22,11 @@ export default function SignupPage() {
       return alert(`${t.please_confirm}`);
     }
 
-    const requestBody = { name, email, password };
+    const requestBody = {name, email, password};
 
     try {
       await postData(`${VITE_API_URL}/auth/signup`, requestBody);
-      navigate("/login");
+      navigate('/login');
     } catch (err) {
       alert(err.message);
     }
@@ -59,27 +38,29 @@ export default function SignupPage() {
 
       <form onSubmit={handleSignupSubmit}>
         <label>Email:</label>
-        <input autoComplete="off" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-
-        {!isCodeSent && (
-          <button type="button" onClick={sendCode}>
-           {t.send_verif_code}
-          </button>
-        )}
-
-        {isCodeSent && !isVerified && (
-          <>
-            <label>Enter your code</label>
-            <input autoComplete="off" type="text" value={code} onChange={(e) => setCode(e.target.value)} />
-            <button type="button" onClick={verifyCode}>{t.confirm_email}</button>
-          </>
-        )}
+        <input
+          autoComplete="off"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>{t.password}</label>
-        <input autoComplete="off" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          autoComplete="off"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <label>{t.name}</label>
-        <input autoComplete="off" type="text" value={name} onChange={(e) => setName(e.target.value)} maxLength={30} />
+        <input
+          autoComplete="off"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          maxLength={30}
+        />
 
         <button type="submit">{t.confirm_sign_up}</button>
       </form>
